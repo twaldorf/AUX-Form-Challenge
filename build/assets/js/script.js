3df38ccd3d007c3f81769a0a7c8d31897f5c3d09
Trevor Waldorf <twaldorf@FTS-users-iMac.example.com>
2016-09-16 12:51:01 -0400

    Add fallback validation, add basic CC validation

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,16 @@
-/* Your JavaScript goes here */
+//This is super not my code, but I do understand it and can step through it.
+//Would like to do a much deeper dive at some point.
+$(function() {
+  $('#card-number').validateCreditCard(function(result) {
+    if(result.card_type == null) {
+      $('#card-number').children().removeClass();
+    } else {
+      $('#' + result.card_type.name).addClass(result.card_type.name);
+    }
+    if(!result.valid) {
+      $('#card-number').removeClass("valid");
+    } else {
+      $('#card-number').addClass("valid");
+    }
+  });
+});
